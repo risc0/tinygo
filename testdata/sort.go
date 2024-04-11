cc0962edebeaf4ef6b5c5538e99f6dcf4ebe3875
Austin Abell <austinabell8@gmail.com>
2024-04-11 15:34:23 -0400

    initial basic writing

--- testdata/sort.go
+++ testdata/sort.go
@@ -1,6 +1,17 @@
 package main
 
-import "sort"
+/*
+#include <stdint.h>
+void sys_halt(uint8_t exit_code, uint32_t* initial_sha_state);
+void sys_write(uint32_t fd, char* c, int len);
+void sys_sha_buffer(uint32_t* out_state, uint32_t* in_state, uint8_t* buf, uint32_t count);
+*/
+import "C"
+import (
+	"encoding/binary"
+	"sort"
+	"unsafe"
+)
 
 // sort.Slice implicitly uses reflect.Swapper
 
@@ -172,6 +183,84 @@ func structs() {
 	}
 }
 
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func shaBuffer(initialState [8]uint32, bytes []byte) [8]uint32 {
+	var outState [8]uint32
+	// TODO doesn't really handle alignment
+	padLen := computeU32sNeeded(len(bytes))
+
+	// Create buffer of u32s to ensure word aligned
+	padBuf := make([]uint32, padLen)
+	if !(len(bytes) <= len(padBuf)*4) {
+		panic("bytes too long")
+	}
+	padBufu8 := unsafe.Slice((*byte)(unsafe.Pointer(&padBuf[0])), 4*len(padBuf))
+
+	// Copy whole bytes
+	length := int(min(len(bytes), padLen*4))
+	copy(padBufu8[:length], bytes[:length])
+
+	// Add END marker since this is always with a trailer
+	padBufu8[length] = 0x80
+
+	// Add trailer with number of bits written. This needs to be big endian.
+	bitsTrailer := 8 * uint32(len(bytes))
+
+	// Swap bits to BE
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint32(b, bitsTrailer)
+	bitsTrailer = uint32(binary.LittleEndian.Uint32(b))
+
+	padBuf[padLen-1] = bitsTrailer
+
+	numBlocks := padLen / 16
+	C.sys_sha_buffer(&outState[0], &initialState[0], &padBufu8[0], uint32(numBlocks))
+	return outState
+}
+
+func taggedStruct(tag string, digests [][8]uint32) [8]uint32 {
+	tagDigest := shaBuffer(sha256InitState(), []byte(tag))
+
+	// Create buffer for all bytes to hash
+	allBytes := make([]byte, 0, len(tagDigest)*4+len(digests)*32+2)
+
+	for _, word := range tagDigest {
+		allBytes = binary.LittleEndian.AppendUint32(allBytes, word)
+	}
+
+	for _, digest := range digests {
+		for _, word := range digest {
+			allBytes = binary.LittleEndian.AppendUint32(allBytes, word)
+		}
+	}
+
+	// TODO doesn't handle extending with data, not needed for this use case yet
+
+	allBytes = binary.LittleEndian.AppendUint16(allBytes, uint16(len(digests)))
+
+	return shaBuffer(sha256InitState(), allBytes)
+}
+
+func sha256InitState() [8]uint32 {
+	// BE conversion of the sha256 init state
+	return [8]uint32{
+		1743128938,
+		2242799547,
+		1928556092,
+		989155237,
+		2136084049,
+		2355627419,
+		2883158815,
+		432922715,
+	}
+}
+
 func main() {
 	strings()
 	int64s()
@@ -180,4 +269,29 @@ func main() {
 	int8s()
 	ints()
 	structs()
+
+	var assumptionsDigestState = [8]uint32{0, 0, 0, 0, 0, 0, 0, 0}
+
+	// No output, but finalize still calls sha on empty bytes it seems
+	journalDigest := shaBuffer(sha256InitState(), []byte{})
+
+	output := taggedStruct("risc0.Output", [][8]uint32{journalDigest, assumptionsDigestState})
+
+	C.sys_halt(0, &output[0])
+}
+
+func computeU32sNeeded(lenBytes int) int {
+	const WordSize = 4
+	const BlockWords = 16
+	// Add one byte for end marker
+	nWords := alignUp(lenBytes+1, WordSize) / WordSize
+	// Add two words for length at end (even though we only
+	// use one of them, being a 32-bit architecture)
+	nWords += 2
+
+	return alignUp(nWords, BlockWords)
+}
+
+func alignUp(addr, al int) int {
+	return (addr + al - 1) & ^(al - 1)
 }
--- testdata/timers.go
+++ testdata/timers.go
@@ -6,14 +6,14 @@ var timer = time.NewTimer(time.Millisecond)
 
 func main() {
 	// Test ticker.
-	ticker := time.NewTicker(time.Millisecond * 500)
+	ticker := time.NewTicker(time.Nanosecond * 5)
 	println("waiting on ticker")
 	go func() {
-		time.Sleep(time.Millisecond * 150)
+		time.Sleep(time.Nanosecond * 5)
 		println(" - after 150ms")
-		time.Sleep(time.Millisecond * 200)
+		time.Sleep(time.Nanosecond * 10)
 		println(" - after 200ms")
-		time.Sleep(time.Millisecond * 300)
+		time.Sleep(time.Nanosecond * 20)
 		println(" - after 300ms")
 	}()
 	<-ticker.C
@@ -21,7 +21,7 @@ func main() {
 	<-ticker.C
 	println("waited on ticker at 1000ms")
 	ticker.Stop()
-	time.Sleep(time.Millisecond * 750)
+	time.Sleep(time.Nanosecond * 7)
 	select {
 	case <-ticker.C:
 		println("fail: ticker should have stopped!")
@@ -29,28 +29,28 @@ func main() {
 		println("ticker was stopped (didn't send anything after 750ms)")
 	}
 
-	timer := time.NewTimer(time.Millisecond * 750)
+	timer := time.NewTimer(time.Nanosecond * 5)
 	println("waiting on timer")
 	go func() {
-		time.Sleep(time.Millisecond * 200)
+		time.Sleep(time.Nanosecond * 2)
 		println(" - after 200ms")
-		time.Sleep(time.Millisecond * 400)
+		time.Sleep(time.Nanosecond * 4)
 		println(" - after 400ms")
 	}()
 	<-timer.C
-	println("waited on timer at 750ms")
-	time.Sleep(time.Millisecond * 500)
+	println("waited on timer at 500ms")
+	time.Sleep(time.Nanosecond * 5)
 
-	reset := timer.Reset(time.Millisecond * 750)
+	reset := timer.Reset(time.Nanosecond * 5)
 	println("timer reset:", reset)
 	println("waiting on timer")
 	go func() {
-		time.Sleep(time.Millisecond * 200)
+		time.Sleep(time.Nanosecond * 2)
 		println(" - after 200ms")
-		time.Sleep(time.Millisecond * 400)
+		time.Sleep(time.Nanosecond * 4)
 		println(" - after 400ms")
 	}()
 	<-timer.C
-	println("waited on timer at 750ms")
-	time.Sleep(time.Millisecond * 500)
+	println("waited on timer at 500ms")
+	time.Sleep(time.Nanosecond * 5)
 }
